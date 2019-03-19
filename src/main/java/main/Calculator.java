9d332b0506e1a63903edaf65eab0f6a066bc83e1
mahender reddy <mahender reddy@192.168.100.7>
2019-03-19 20:46:38 +0530

    hai

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -9,7 +9,6 @@ public class Calculator {
         int num1,num2;
         num1 = inp.nextInt();
         num2 = inp.nextInt();
-        int ans;
         System.out.println("Enter your selection: 1 for Addition, 2 for substraction 3 for Multiplication and 4 for division:");
         int choose;
         choose = inp.nextInt();
@@ -39,6 +38,7 @@ public class Calculator {
     {
     	int result=0;
 //        write your code here 
+    	result = x+y;
     	
         return result;
     }
@@ -46,18 +46,21 @@ public class Calculator {
     {
     	int result=0;
 //        write your code here
+    	result = x-y;
         return result;
     }
     public static int mult(int x, int y)
     {
     	int result=0;
-//      write your code here        
+//      write your code here  
+    	result = x*y;
     	return result;
     }
     public static int div(int x, int y)
     {
     	int result=0;
 //      write your code here
+    	result = x/y;
     	return result;
     }
 
